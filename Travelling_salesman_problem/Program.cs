fd93c430e1af17d51a525635fac9a8444cd2cd58
Boris Pavlov <borzpavlov@gmail.com>
2021-12-20 08:43:27 +0500

    Add project files.

---
+++ Travelling_salesman_problem/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Travelling_salesman_problem {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
